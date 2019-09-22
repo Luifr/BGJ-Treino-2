3c57e97cde4bab27e0c8fa837f9f6067d0dad107
luifr <luifr@usp.br>
2019-09-22 20:19:01 -0300

    order float text

--- Assets/Scripts/NpcBehaviour.cs
+++ Assets/Scripts/NpcBehaviour.cs
@@ -8,6 +8,9 @@ public class NPCBehaviour : MonoBehaviour
 	Rigidbody2D body;
 
 	[SerializeField]
+	GameObject orderPrefab;
+
+	[SerializeField]
 	float runSpeed = 5.0f;
 	public bool makeOrder;
 	public int stopIndex;
@@ -20,7 +23,7 @@ public class NPCBehaviour : MonoBehaviour
 		}
 		else
 		{
-			StartCoroutine(MakeOrder());
+			StartCoroutine(MoveToOrder());
 		}
 
 	}
@@ -30,9 +33,20 @@ public class NPCBehaviour : MonoBehaviour
 		body.velocity = move * runSpeed;
 	}
 
-	IEnumerator MakeOrder()
+	IEnumerator MoveToOrder()
 	{
 		yield return new WaitForSeconds(move.y > 0 ? (1.2f + stopIndex * 0.5f) : (1.8f - stopIndex * 0.5f));
 		move = new Vector2(-1, 0);
+		yield return new WaitForSeconds(1f);
+		MakeOrder();
+		move = new Vector2(0, 0);
 	}
+
+	void MakeOrder()
+	{
+		GameObject order = Instantiate(orderPrefab, new Vector2(-0, 0), Quaternion.identity);
+		order.transform.parent = transform;
+		order.transform.localPosition = new Vector2(-1f, 1f);
+	}
+
 }
