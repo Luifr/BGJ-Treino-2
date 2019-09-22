e3992bdf8d1e2bc59e6a1d96be049dff51117d91
luifr <luifr@usp.br>
2019-09-22 18:40:57 -0300

    Spawners, npc

---
+++ Assets/Scripts/NPCSpawnerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawnerController : MonoBehaviour
+{
+
+	Spawner[] spawners;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		spawners = transform.GetComponentsInChildren<Spawner>();
+		foreach (Spawner spawner in spawners)
+		{
+			spawner.InstatiateCallBack = InitialiseNpc;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void InitialiseNpc(GameObject gameObject)
+	{
+
+	}
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	[SerializeField]
+	GameObject prefab;
+	[SerializeField]
+	Direction direction;
+
+	public delegate void Initialise(GameObject gameObject);
+	public Initialise InstatiateCallBack = null;
+
+	public void Spawn(Vector3 position)
+	{
+		Instantiate(prefab, position, Quaternion.identity);
+	}
+
+	public void Spawn()
+	{
+		GameObject gameObject = Instantiate(prefab, transform.position, Quaternion.identity);
+		if (InstatiateCallBack != null)
+		{
+			InstatiateCallBack(gameObject);
+		}
+	}
+
+
+	public enum Direction
+	{
+		up,
+		left,
+		down,
+		right
+	}
+}
